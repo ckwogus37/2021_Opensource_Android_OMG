30de037fd65cff03ae6337e211f52aaedf60412e
shin <shin5513976@naver.com>
2021-05-29 20:57:03 +0900

    1.2

--- firstCBNU/firstCBNU/app/src/main/java/com/example/firstcbnu/Search.java
+++ firstCBNU/firstCBNU/app/src/main/java/com/example/firstcbnu/Search.java
@@ -2,16 +2,23 @@ package com.example.firstcbnu;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.AdapterView;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,9 +39,23 @@ public class Search extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);    //listview가 있는 xml과 연결
-
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher_foreground);
         lv = (ListView) findViewById(R.id.list);    //search.xml파일에 있는 listview를 선언
         sv = (SearchView) findViewById(R.id.search_bar);    //search.xml파일에 있는 searchview를 선언
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                VoiceTask voiceTask = new VoiceTask();
+                voiceTask.execute();
+            }
+        });
+
 
         showlist= new ArrayList<>();    //listview에 보여질 대학들의 배열리스트
 
@@ -76,6 +97,59 @@ public class Search extends AppCompatActivity {
             }
         });
 
+
+    }
+    public class VoiceTask extends AsyncTask<String, Integer, String> {
+        String str = null;
+
+        @Override
+        protected String doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            try {
+                getVoice();
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+            return str;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+
+            } catch (Exception e) {
+                Log.d("onActivityResult", "getImageURL exception");
+            }
+        }
+    }
+
+    private void getVoice() {
+
+        Intent intent = new Intent();
+        intent.setAction(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        String language = "ko-KR";
+
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, language);
+        startActivityForResult(intent, 2);
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            ArrayList<String> results = data
+                    .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+            String str = results.get(0);
+            adapter.getFilter().filter(str);
+            Toast.makeText(getBaseContext(), str +"이(가) 입력되었습니다.", Toast.LENGTH_LONG).show();
+        }
     }
 
 
